fec9fe88b528944c5ecfaedef0449f1e6de81b2d
Nikhil Raote <nikhilraote0@gmail.com>
2026-03-15 12:48:36 +0530

    selenium demo project

---
+++ src/test/java/SeleniumSearchDemo.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumSearchDemo {
+
+    @Test
+    public void seleniumDemo() throws InterruptedException {
+
+        System.out.println("Starting Selenium Demo...");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.name("q")).sendKeys("Selenium automation tutorial");
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.name("q")).submit();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("(//h3)[1]")).click();
+
+        Thread.sleep(5000);
+
+        System.out.println("First result opened successfully");
+
+        driver.quit();
+    }
+}
